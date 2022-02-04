f8734227e0d69ecc70921b3e22b61b2af24dd25c
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2022-02-04 15:05:37 -0500

    Update config.php

--- system/modules/page-tag-navigation/config/config.php
+++ system/modules/page-tag-navigation/config/config.php
@@ -11,50 +11,14 @@
  */
 
 
-
-/* Back end modules */
-// DECLARING LOCATIONS BACK END PLUGIN
-if (!is_array($GLOBALS['BE_MOD']['panel_pricing_calculator']))
-{
-    array_insert($GLOBALS['BE_MOD'], 1, array('panel_pricing_calculator' => array()));
-}
-
 // This sets up our custom section "PANEL PRICING CALCULATOR"
-$GLOBALS['TL_LANG']['MOD']['panel_pricing_calculator'][0] = "Panel Pricing Calculator";
+$GLOBALS['TL_LANG']['MOD']['page-tag-navigation'][0] = "Page Tag Navigation";
 // Adds "Panel Pricing Calculator" to our custom section
-$GLOBALS['BE_MOD']['panel_pricing_calculator']['price_chart'] = array(
-	'tables' => array('tl_price_chart'),
-	'icon'   => 'system/modules/panel_pricing_calculator/assets/icons/panel_pricing_calculator.png',
-	'exportLocations' => array('Bcs\Backend\PanelPricingCalculatorBackend', 'exportPanelPricingCalculator')
+$GLOBALS['BE_MOD']['page-tag-navigation']['parent_category'] = array(
+	'tables' => array('tl_parent_category'),
+	'icon'   => 'system/modules/page-tag-navigation/assets/icons/page_tag_navigation.png',
+	'exportLocations' => array('Bcs\Backend\ParentCategoryBackend', 'exportParentCategory')
 );
-$GLOBALS['BE_MOD']['panel_pricing_calculator']['price_chart_large'] = array(
-	'tables' => array('tl_price_chart_large'),
-	'icon'   => 'system/modules/panel_pricing_calculator/assets/icons/panel_pricing_calculator.png',
-	'exportLocations' => array('Bcs\Backend\PanelPricingCalculatorBackend', 'exportPanelPricingCalculator')
-);
-
-$GLOBALS['BE_MOD']['panel_pricing_calculator']['cradle_prices'] = array(
-	'tables' => array('tl_cradle_prices'),
-	'icon'   => 'system/modules/panel_pricing_calculator/assets/icons/panel_pricing_calculator.png',
-	'exportLocations' => array('Bcs\Backend\PanelPricingCalculatorBackend', 'exportPanelPricingCalculator')
-);
-
-$GLOBALS['BE_MOD']['panel_pricing_calculator']['quote_request'] = array(
-	'tables' => array('tl_quote_request'),
-	'icon'   => 'system/modules/panel_pricing_calculator/assets/icons/panel_pricing_calculator.png',
-	'exportLocations' => array('Bcs\Backend\PanelPricingCalculatorBackend', 'exportPanelPricingCalculator')
-);
-
-/* Front end modules */
-$GLOBALS['FE_MOD']['panel_pricing_calculator']['panel_pricing_calculator_module'] 	= 'Bcs\Module\PanelPricingCalculatorModule';
-
 
 /* Models */
-$GLOBALS['TL_MODELS']['tl_panel_pricing_calculator'] = 'Bcs\Model\PanelPricingCalculator';
-
-
-/* Add Backend CSS to style Reviewed and Unreviewed */
-if (TL_MODE == 'BE')
-{
-	$GLOBALS['TL_CSS'][]					= 'system/modules/panel_pricing_calculator/assets/css/panel_pricing_calculator.css';
-}
+$GLOBALS['TL_MODELS']['tl_page_tag_navigation'] = 'Bcs\Model\PageTagNavigation';
