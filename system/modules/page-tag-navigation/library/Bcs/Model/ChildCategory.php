2dd840e0fee47d21ee082569d8856813b34d445a
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2022-02-04 15:43:05 -0500

    Create ChildCategory.php

---
+++ system/modules/page-tag-navigation/library/Bcs/Model/ChildCategory.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * Locations - Location Plugin for Contao
+ *
+ * Copyright (C) 2018 Andrew Stevens
+ *
+ * @package    asconsulting/locations
+ * @link       http://andrewstevens.consulting
+ * @license    http://opensource.org/licenses/lgpl-3.0.html
+ */
+
+ 
+namespace Bcs\Model;
+
+class ChildCategory extends \Model
+{
+	
+	/**
+	 * Table name
+	 * @var string
+	 */
+	protected static $strTable = 'tl_child_category';
+
+	
+}
