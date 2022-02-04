39f635c6b1c89fe31a28509ec74a45f10c26b855
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2022-02-04 15:38:01 -0500

    Create tl_child_category.php

---
+++ system/modules/page-tag-navigation/dca/tl_child_category.php
@@ -0,0 +1,168 @@
+<?php
+
+/**
+ * Locations - Location Plugin for Contao
+ *
+ * Copyright (C) 2018 Andrew Stevens
+ *
+ * @package    asconsulting/locations
+ * @link       http://andrewstevens.consulting
+ * @license    http://opensource.org/licenses/lgpl-3.0.html
+ */
+
+ 
+/**
+ * Table tl_location
+ */
+$GLOBALS['TL_DCA']['tl_child_category'] = array
+(
+ 
+    // Config
+    'config' => array
+    (
+        'dataContainer'               => 'Table',
+        'enableVersioning'            => true,
+        'sql' => array
+        (
+            'keys' => array
+            (
+                'id' 	=> 	'primary',
+                'pid' 	=> 	'index',
+                'alias' =>  'index'
+            )
+        )
+    ),
+ 
+    // List
+    'list' => array
+    (
+        'sorting' => array
+        (
+            'mode'                    => 1,
+            'fields'                  => array('label'),
+            'flag'                    => 1,
+            'panelLayout'             => 'filter;search,limit'
+        ),
+        'label' => array
+        (
+            'fields'                  => array('label'),
+            'format'                  => '%s'
+        ),
+        'global_operations' => array
+        (
+            'export' => array
+            (
+                'label'               => 'Export Locations CSV',
+                'href'                => 'key=exportLocations',
+                'icon'                => 'system/modules/page-tag-navigation/assets/icons/file-export-icon-16.png'
+            ),
+            'all' => array
+            (
+                'label'               => &$GLOBALS['TL_LANG']['MSC']['all'],
+                'href'                => 'act=select',
+                'class'               => 'header_edit_all',
+                'attributes'          => 'onclick="Backend.getScrollOffset()" accesskey="e"'
+            )
+
+        ),
+        'operations' => array
+        (
+            'edit' => array
+            (
+                'label'               => &$GLOBALS['TL_LANG']['tl_child_category']['edit'],
+                'href'                => 'act=edit',
+                'icon'                => 'edit.gif'
+            ),
+			
+            'copy' => array
+            (
+                'label'               => &$GLOBALS['TL_LANG']['tl_child_category']['copy'],
+                'href'                => 'act=copy',
+                'icon'                => 'copy.gif'
+            ),
+            'delete' => array
+            (
+                'label'               => &$GLOBALS['TL_LANG']['tl_child_category']['delete'],
+                'href'                => 'act=delete',
+                'icon'                => 'delete.gif',
+                'attributes'          => 'onclick="if(!confirm(\'' . $GLOBALS['TL_LANG']['MSC']['deleteConfirm'] . '\'))return false;Backend.getScrollOffset()"'
+            ),
+            'toggle' => array
+            (
+                'label'               => &$GLOBALS['TL_LANG']['tl_child_category']['toggle'],
+                'icon'                => 'visible.gif',
+                'attributes'          => 'onclick="Backend.getScrollOffset();return AjaxRequest.toggleVisibility(this,%s)"',
+                'button_callback'     => array('Bcs\Backend\ChildCategoryBackend', 'toggleIcon')
+            ),
+            'show' => array
+            (
+                'label'               => &$GLOBALS['TL_LANG']['tl_child_category']['show'],
+                'href'                => 'act=show',
+                'icon'                => 'show.gif'
+            )
+        )
+    ),
+ 
+    // Palettes
+    'palettes' => array
+    (
+        'default'                     => '{location_legend},label;{category_legend},pid;{publish_legend},published;'
+    ),
+ 
+    // Fields
+    'fields' => array
+    (
+        'id' => array
+        (
+            'sql'                     => "int(10) unsigned NOT NULL auto_increment"
+        ),
+		
+		    'pid' => array
+        (
+            'sql'                     => "varchar(255) NOT NULL default ''",
+			      'label'                   => &$GLOBALS['TL_LANG']['tl_child_category']['categories'],
+			      'inputType'               => 'checkbox',
+			      'options_callback'		  => array('Bcs\Backend\ChildCategoryBackend', 'getParentCategories'),										
+			      'eval'                    => array('multiple'=>true, 'mandatory'=>true,'tl_class'=>'clr') 
+        ),
+        'tstamp' => array
+        (
+            'sql'                     => "int(10) unsigned NOT NULL default '0'"
+        ),
+		'sorting' => array
+		(
+			'sql'                     => "int(10) unsigned NOT NULL default '0'"
+		),
+		'alias' => array
+		(
+			'label'                   => &$GLOBALS['TL_LANG']['tl_child_category']['alias'],
+			'exclude'                 => true,
+			'inputType'               => 'text',
+			'search'                  => true,
+			'eval'                    => array('unique'=>true, 'rgxp'=>'alias', 'doNotCopy'=>true, 'maxlength'=>128, 'tl_class'=>'w50'),
+			'save_callback' => array
+			(
+			      array('Bcs\Backend\ChildCategoryBackend', 'generateAlias')
+			),
+			'sql'                     => "varchar(128) COLLATE utf8_bin NOT NULL default ''"
+
+		),
+		'label' => array
+		(
+			'label'                   => &$GLOBALS['TL_LANG']['tl_child_category']['label'],
+			'inputType'               => 'text',
+			'default'				  => '',
+			'search'                  => true,
+			'eval'                    => array('mandatory'=>true, 'tl_class'=>'clr w50'),
+			'sql'                     => "varchar(255) NOT NULL default ''"
+		),
+		'published' => array
+		(
+			'exclude'                 => true,
+			'label'                   => &$GLOBALS['TL_LANG']['tl_child_category']['published'],
+			'inputType'               => 'checkbox',
+			'eval'                    => array('submitOnChange'=>true, 'doNotCopy'=>true),
+			'sql'                     => "char(1) NOT NULL default ''"
+		)		
+    )
+);
