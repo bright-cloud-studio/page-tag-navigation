cbda14e38fdf84ab8be6a4c0890abda150dd6382
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2022-02-04 15:08:43 -0500

    Update and rename tl-parent-category.php to tl_parent_category.php

---
+++ system/modules/page-tag-navigation/languages/en/tl_parent_category.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * Locations - Location Plugin for Contao
+ *
+ * Copyright (C) 2018 Andrew Stevens
+ *
+ * @package    asconsulting/locations
+ * @link       http://andrewstevens.consulting
+ * @license    http://opensource.org/licenses/lgpl-3.0.html
+ */
+
+
+/**
+ * Legends
+ */
+$GLOBALS['TL_LANG']['tl_parent_category']['category_legend'] 	= 'Parent Category Details';
+$GLOBALS['TL_LANG']['tl_parent_category']['publish_legend'] 	= 'Publish';
+
+/**
+ * Fields
+ */
+$GLOBALS['TL_LANG']['tl_parent_category']['alias'] 			= array('Alias', 'Auto-generated alias.');
+$GLOBALS['TL_LANG']['tl_parent_category']['label'] 				= array('Label', 'Parent category label');
+$GLOBALS['TL_LANG']['tl_parent_category']['published'] 		= array('Published', 'Show this record on the front end.');
+ 
+ /**
+ * Buttons
+ */
+$GLOBALS['TL_LANG']['tl_parent_category']['new']   			= array('New record', 'Add a new record');
+$GLOBALS['TL_LANG']['tl_parent_category']['show']  		 	= array('Record details', 'Show the details of record ID %s');
+$GLOBALS['TL_LANG']['tl_parent_category']['edit']  		 	= array('Edit record', 'Edit record ID %s');
+$GLOBALS['TL_LANG']['tl_parent_category']['copy']  		 	= array('Copy record', 'Copy record ID %s');
+$GLOBALS['TL_LANG']['tl_parent_category']['delete'] 			= array('Delete record', 'Delete record ID %s');
+$GLOBALS['TL_LANG']['tl_parent_category']['toggle'] 			= array('Toggle record', 'Toggle record ID %s');
