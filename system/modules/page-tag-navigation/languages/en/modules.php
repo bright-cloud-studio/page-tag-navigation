960ee75e504d2cf8e97c7fcac0ea01c7838a72c5
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2022-02-04 15:25:26 -0500

    Create modules.php

---
+++ system/modules/page-tag-navigation/languages/en/modules.php
@@ -0,0 +1,24 @@
+
+<?php
+
+/**
+ * Locations - Location Plugin for Contao
+ *
+ * Copyright (C) 2018 Andrew Stevens
+ *
+ * @package    asconsulting/locations
+ * @link       http://andrewstevens.consulting
+ * @license    http://opensource.org/licenses/lgpl-3.0.html
+ */
+
+ 
+/**
+ * Front end modules
+ */
+//$GLOBALS['TL_LANG']['FMD']['locations_list'] 	= array('Locations List', 'Locations list module.');
+
+
+/**
+ * Back end modules
+ */
+$GLOBALS['TL_LANG']['MOD']['parent_categories'] = array('Parent Categories', 'Define Parent Categories');
