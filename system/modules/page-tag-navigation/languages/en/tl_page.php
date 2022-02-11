3f4fc14c0dcd579913f7d556c21a2ef9b28582cf
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2022-02-11 13:44:31 -0500

    Create tl_page.php

---
+++ system/modules/page-tag-navigation/languages/en/tl_page.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Bright Cloud Studio's Page Tag Navigation
+ *
+ * Copyright (C) 2022 Bright Cloud Studio
+ *
+ * @package    bright-cloud-studio/page-tag-navigation
+ * @link       https://www.brightcloudstudio.com/
+ * @license    http://opensource.org/licenses/lgpl-3.0.html
+**/
+
+
+/* Fields */
+$GLOBALS['TL_LANG']['tl_page']['page_tag_redirect']          = array('Page Tag Redirect', 'Which option in the Page Tag Navigation will redirect to this page.');
+
+/* Legends */
+$GLOBALS['TL_LANG']['tl_user']['page_tag_redirect_navigation'] = 'Page Tag Navigation\'s Settings';
