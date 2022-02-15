543520981b16ae8f17ab52b82c3bc3ff7b9db0aa
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2022-02-15 13:18:06 -0500

    Create tl_module.php

---
+++ system/modules/page-tag-navigation/languages/en/tl_module.php
@@ -0,0 +1,7 @@
+<?php
+
+/* Fields */
+//$GLOBALS['TL_LANG']['tl_module']['selectedGallery'] 		= array('Galleries Available', 'Select which available gallery this front end module will display.');
+
+/* Legends */
+//$GLOBALS['TL_LANG']['tl_module']['select_gallery_legend'] 	= "Select a Gallery";
