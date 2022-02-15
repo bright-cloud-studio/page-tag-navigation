0ea8bd4426ab2656c87709c27ca01ba931c51310
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2022-02-15 14:30:33 -0500

    Update PageTagNavigationModule.php

--- system/modules/page-tag-navigation/library/Bcs/Module/PageTagNavigationModule.php
+++ system/modules/page-tag-navigation/library/Bcs/Module/PageTagNavigationModule.php
@@ -67,15 +67,10 @@ class PageTagNavigationModule extends \Contao\Module
      */
     protected function compile()
     {
-
-	    // add our css
-	    if (!in_array('system/modules/modal_gallery/assets/css/modal_gallery.css', $GLOBALS['TL_CSS'])) { 
-        $GLOBALS['TL_CSS'][] = 'system/modules/modal_gallery/assets/css/modal_gallery.css';
-      }
-	    // add our js
-	    if (!in_array('<script src="system/modules/modal_gallery/assets/js/modal_gallery.js"></script>', $GLOBALS['TL_BODY'])) { 
-			  $GLOBALS['TL_BODY'][] = '<script src="system/modules/modal_gallery/assets/js/modal_gallery.js"></script>';
-		  }
+		// add our js
+		if (!in_array('<script src="system/modules/page-tag-navigation/assets/js/page_tag_navigation.js"></script>', $GLOBALS['TL_BODY'])) { 
+			$GLOBALS['TL_BODY'][] = '<script src="system/modules/page-tag-navigation/assets/js/page_tag_navigation.js"></script>';
+		}
 	    
 	    /*
 		$objLocation = Location::findBy('published', '1');
