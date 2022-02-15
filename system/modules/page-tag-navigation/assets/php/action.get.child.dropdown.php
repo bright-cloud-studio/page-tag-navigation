dcbf406b56493c5ef1dfd8729fb2fb3d5d8211f1
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2022-02-15 15:06:43 -0500

    Update action.get.child.dropdown.php

--- system/modules/page-tag-navigation/assets/php/action.get.child.dropdown.php
+++ system/modules/page-tag-navigation/assets/php/action.get.child.dropdown.php
@@ -6,5 +6,20 @@ include_once("prepend.page_tag_navigation.endpoint.php");
 $vars = $_POST;
 
 
+$dbh = new mysqli("localhost", "ampersan_dbadmin", "Y06ZCg9BiAh2Uv#@", "ampersan_cms49");
+if ($dbh->connect_error) {
+  die("Connection failed: " . $dbh->connect_error);
+}
+
+$sorting_number = 0;
+$query =  "select * from tl_quote_request ORDER BY sorting desc LIMIT 1";
+$result = $dbh->query($query);
+if($result) {
+  while($row = $result->fetch_assoc()) {
+    $sorting_number = $row['sorting'];
+  }
+}
+
+
 $message = "Ding" . $vars['parent_val'];
 echo $message;
