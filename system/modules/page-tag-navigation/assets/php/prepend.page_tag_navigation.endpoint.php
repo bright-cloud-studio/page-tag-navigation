592375001c2da67bd2e9b10582eaa4a7cc0e90c5
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2022-02-15 14:51:53 -0500

    Update prepend.page_tag_navigation.endpoint.php

--- system/modules/page-tag-navigation/assets/php/prepend.page_tag_navigation.endpoint.php
+++ system/modules/page-tag-navigation/assets/php/prepend.page_tag_navigation.endpoint.php
@@ -1,9 +1,5 @@
 <?php
 
-// start a session
-session_start();
-error_reporting(E_ALL ^ E_NOTICE);
-
 try {
     $dbh = new PDO("mysql:host=localhost;dbname=oces_contao_4_9", 'oces_user', 'cnLtU3L0fD8PNurD)R', array(
     PDO::ATTR_PERSISTENT => true
