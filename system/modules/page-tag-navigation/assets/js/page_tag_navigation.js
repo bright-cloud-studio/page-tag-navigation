f3943ff141ec27427bb4662e6dd82d51992ad805
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2022-02-15 14:34:40 -0500

    Update page_tag_navigation.js

--- system/modules/page-tag-navigation/assets/js/page_tag_navigation.js
+++ system/modules/page-tag-navigation/assets/js/page_tag_navigation.js
@@ -2,14 +2,14 @@
 function get_child_dropdown(){
 
     // store our quote request values
-    var val_parent = $("#dropdown_parent").val();
+    var parent_val = $("#dropdown_parent").val();
 
 	  
     // trigger this function when our form runs
     $.ajax({
         url:'/system/modules/panel_pricing_calculator/assets/php/action.get.child.dropdown.php',
         type:'POST',
-        data:"val_parent="+val_parent+"",
+        data:"val_parent="+parent_val+"",
         success:function(result){
         	//$("#send_email_notification").html(result);
         	//$("#request_form").hide();
