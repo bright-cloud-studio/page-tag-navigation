7875691e9361b324a628828e8108903dc23aa978
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2022-02-15 14:43:07 -0500

    Update action.get.child.dropdown.php

--- system/modules/page-tag-navigation/assets/php/action.get.child.dropdown.php
+++ system/modules/page-tag-navigation/assets/php/action.get.child.dropdown.php
@@ -6,9 +6,4 @@ include_once("prepend.page_tag_navigation.endpoint.php");
 // object created in prepend.endpoint.php, set our total price to the value returned from the processForm() function
 $cart_total = $QuoteCart->addToCart();
 
-// if we have a total price, push it onto the page to be grabbed by the ajax call
-if ($cart_total) {
-    ?>
-<?php echo $cart_total; ?>
-<?php
-} ?>
+echo "Ding!";
