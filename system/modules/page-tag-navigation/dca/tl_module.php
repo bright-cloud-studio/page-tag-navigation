87816dd2123bf1ed5f0ee82e28bd77f8623591a0
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2022-02-15 13:13:46 -0500

    Create tl_module.php

---
+++ system/modules/page-tag-navigation/dca/tl_module.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Bright Cloud Studio's Page Tag Navigation
+ *
+ * Copyright (C) 2022 Bright Cloud Studio
+ *
+ * @package    bright-cloud-studio/page-tag-navigation
+ * @link       https://www.brightcloudstudio.com/
+ * @license    http://opensource.org/licenses/lgpl-3.0.html
+**/
+
+/* Add a palette to tl_module */
+$GLOBALS['TL_DCA']['tl_module']['palettes']['page_tag_navigation_module'] 		= '{title_legend},name,headline,type;{template_legend:hide},customTpl;{expert_legend:hide},guests,cssID,space';
