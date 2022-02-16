b7d0f2cbda61d6bbed93494daa1116ed1357f3ba
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2022-02-16 14:29:46 -0500

    Create tl_settings.php

---
+++ system/modules/page-tag-navigation/dca/tl_settings.php
@@ -0,0 +1,27 @@
+<?php
+
+
+
+
+$GLOBALS['TL_DCA']['tl_settings']['palettes']['default'] = str_replace(';{date_legend', ';{page_tag_navigation_defaults_legend},parentDefault,childDefault;{date_legend', $GLOBALS['TL_DCA']['tl_settings']['palettes']['default']);
+
+
+/**
+ * Article Fields
+ */
+$GLOBALS['TL_DCA']['tl_settings']['fields']['parentDefault'] = array(
+	'label'                   => &$GLOBALS['TL_LANG']['tl_settings']['parentDefault'],
+	'inputType'               => 'text',
+  'default'		              => 'I am...',
+  'search'                  => true,
+  'eval'                    => array('mandatory'=>true, 'tl_class'=>'w50'),
+  'sql'                     => "varchar(255) NOT NULL default ''"
+);
+$GLOBALS['TL_DCA']['tl_settings']['fields']['childDefault'] = array(
+	'label'                   => &$GLOBALS['TL_LANG']['tl_settings']['childDefault'],
+	'inputType'               => 'text',
+  'default'		              => 'I\'m looking for...',
+  'search'                  => true,
+  'eval'                    => array('mandatory'=>true, 'tl_class'=>'w50'),
+  'sql'                     => "varchar(255) NOT NULL default ''"
+);
