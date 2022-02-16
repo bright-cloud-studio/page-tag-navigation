482029318929b52a3e6d3f5d277f3c7c9e534482
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2022-02-16 15:20:27 -0500

    Update tl_page.php

--- system/modules/page-tag-navigation/languages/en/tl_page.php
+++ system/modules/page-tag-navigation/languages/en/tl_page.php
@@ -11,8 +11,9 @@
 **/
 
 
-/* Fields */
-$GLOBALS['TL_LANG']['tl_page']['page_tag_navigation_target']          = array('Link to Child Option', 'Which of the Page Tag Navigation\'s child option will this redirect the user to this page.');
-
 /* Legends */
 $GLOBALS['TL_LANG']['tl_page']['page_tag_navigation_legend'] = 'Page Tag Navigation\'s Settings';
+
+/* Fields */
+$GLOBALS['TL_LANG']['tl_page']['page_tag_navigation_target']          = array('Link to Child Option', 'Which of the Page Tag Navigation\'s child option will this redirect the user to this page.');
+$GLOBALS['TL_LANG']['tl_page']['page_tag_navigation_anchor_target']   = array('Anchor Target', 'Anchor Target for which place on the page the redirect will jump to');
