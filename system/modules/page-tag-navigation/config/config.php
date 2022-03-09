99b112028189d02ee7c100d802f70aae9b004315
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2022-03-09 11:21:33 -0500

    Update config.php

--- system/modules/page-tag-navigation/config/config.php
+++ system/modules/page-tag-navigation/config/config.php
@@ -1,20 +1,17 @@
 <?php
 
 /**
- * Panel Pricing Calculator - Adds a panel pricing calculator with a front end module for users and a back end system for managing the data.
+ * Bright Cloud Studio's Page Tag Navigation
  *
- * Copyright (C) 2021 Bright Cloud Studio
+ * Copyright (C) 2022 Bright Cloud Studio
  *
- * @package    bright-cloud-studio/panel-pricing-calculator
+ * @package    bright-cloud-studio/page-tag-navigation
  * @link       https://www.brightcloudstudio.com/
  * @license    http://opensource.org/licenses/lgpl-3.0.html
- */
+**/
 
-
-// This sets up our custom section "PANEL PRICING CALCULATOR"
 $GLOBALS['TL_LANG']['MOD']['page-tag-navigation'][0] = "Page Tag Navigation";
 
-// Adds "Panel Pricing Calculator" to our custom section
 $GLOBALS['BE_MOD']['page-tag-navigation']['parent_category'] = array(
 	'tables' => array('tl_parent_category'),
 	'icon'   => 'system/modules/page-tag-navigation/assets/icons/page_tag_navigation.png',
