65814c45ffda251b85c5b59b275d893ab4fcceb2
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2022-03-09 11:24:32 -0500

    Update tl_settings.php

--- system/modules/page-tag-navigation/languages/en/tl_settings.php
+++ system/modules/page-tag-navigation/languages/en/tl_settings.php
@@ -1,12 +1,18 @@
 <?php
 
 /**
- * Legends
- */
+ * Bright Cloud Studio's Page Tag Navigation
+ *
+ * Copyright (C) 2022 Bright Cloud Studio
+ *
+ * @package    bright-cloud-studio/page-tag-navigation
+ * @link       https://www.brightcloudstudio.com/
+ * @license    http://opensource.org/licenses/lgpl-3.0.html
+**/
+
+/* Legends */
 $GLOBALS['TL_LANG']['tl_settings']['page_tag_navigation_default_legend'] 				= 'Page Tag Navigation Default Settings';
 
-/**
- * Article Fields
- */
+/* Article Fields */
 $GLOBALS['TL_LANG']['tl_settings']['parentDefault'] 			= array('Parent Dropdown Default Option', 'The default option for the Parent dropdown');
 $GLOBALS['TL_LANG']['tl_settings']['childDefault'] 			= array('Child Dropdown Default Option', 'The default option for the Child dropdown');
