94d0bb159b3e5de1d1a0aefcc8c8f77391595f53
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2022-03-09 11:22:49 -0500

    Update tl_settings.php

--- system/modules/page-tag-navigation/dca/tl_settings.php
+++ system/modules/page-tag-navigation/dca/tl_settings.php
@@ -1,14 +1,20 @@
 <?php
 
-
+/**
+ * Bright Cloud Studio's Page Tag Navigation
+ *
+ * Copyright (C) 2022 Bright Cloud Studio
+ *
+ * @package    bright-cloud-studio/page-tag-navigation
+ * @link       https://www.brightcloudstudio.com/
+ * @license    http://opensource.org/licenses/lgpl-3.0.html
+**/
 
 
 $GLOBALS['TL_DCA']['tl_settings']['palettes']['default'] = str_replace(';{date_legend', ';{page_tag_navigation_default_legend},parentDefault,childDefault;{date_legend', $GLOBALS['TL_DCA']['tl_settings']['palettes']['default']);
 
 
-/**
- * Article Fields
- */
+/* Article Fields */
 $GLOBALS['TL_DCA']['tl_settings']['fields']['parentDefault'] = array(
 	'label'                   => &$GLOBALS['TL_LANG']['tl_settings']['parentDefault'],
 	'inputType'               => 'text',
