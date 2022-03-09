590091cf09c67144f8f34be41f095227ece3cccd
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2022-03-09 11:22:22 -0500

    Update tl_page.php

--- system/modules/page-tag-navigation/dca/tl_page.php
+++ system/modules/page-tag-navigation/dca/tl_page.php
@@ -13,20 +13,6 @@
  /* Extend the tl_user palettes */
 $GLOBALS['TL_DCA']['tl_page']['palettes']['regular'] = str_replace(';{publish_legend}', ';{page_tag_navigation_legend},page_tag_navigation_target,page_tag_navigation_anchor_target;{publish_legend}', $GLOBALS['TL_DCA']['tl_page']['palettes']['regular']);
 
-
-/*
-$GLOBALS['TL_DCA']['tl_page']['fields']['page_tag_navigation_target'] = array
-(
-	'label'			=> &$GLOBALS['TL_LANG']['tl_page']['page_tag_navigation_target'],
-	'inputType'		=> 'textarea',
-	'eval'                	=> [
-		'rte'=>'tinyMCE',
-		'tl_class'=>'long'
-	],
-	'sql'                   => "mediumtext NOT NULL default ''"
-);
-*/
-
 $GLOBALS['TL_DCA']['tl_page']['fields']['page_tag_navigation_target'] = array
 (
 	'sql'                   => "varchar(255) NOT NULL default ''",
