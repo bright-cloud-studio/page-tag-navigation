3127f86c4527c11c5dd571864aa4a4f9b88d88a0
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2022-03-09 11:23:41 -0500

    Update tl_module.php

--- system/modules/page-tag-navigation/languages/en/tl_module.php
+++ system/modules/page-tag-navigation/languages/en/tl_module.php
@@ -1,5 +1,15 @@
 <?php
 
+/**
+ * Bright Cloud Studio's Page Tag Navigation
+ *
+ * Copyright (C) 2022 Bright Cloud Studio
+ *
+ * @package    bright-cloud-studio/page-tag-navigation
+ * @link       https://www.brightcloudstudio.com/
+ * @license    http://opensource.org/licenses/lgpl-3.0.html
+**/
+
 /* Fields */
 //$GLOBALS['TL_LANG']['tl_module']['selectedGallery'] 		= array('Galleries Available', 'Select which available gallery this front end module will display.');
 
