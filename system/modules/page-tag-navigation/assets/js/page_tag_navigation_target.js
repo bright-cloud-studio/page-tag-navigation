04260b905ce30793947c811795c27425bcbad553
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2022-04-29 12:42:50 -0400

    Update page_tag_navigation_target.js

--- system/modules/page-tag-navigation/assets/js/page_tag_navigation_target.js
+++ system/modules/page-tag-navigation/assets/js/page_tag_navigation_target.js
@@ -1,23 +1,35 @@
-// When the page is loaded, get our target
+// when page is loaded
 $( document ).ready(function() {
+
+	// get our target from the module holder
     var our_target = $('#target_holder').text();
+    
+    // if our target isn't blank
     if(our_target != '') {
+    	
+    	// scroll the page to the target
     	scrollToAnchor(our_target);
+    	
+    	// add bouncing ball
+    	addBouncingBall(our_target);
+    	
+    	// clear our stored target
     	clearSessionTarget();
     }
     else
+    	// we have no target
     	console.log("No Target");
 });
 
-
+// scroll the page to the target
 function scrollToAnchor(aid){
     var aTag = $("div[id='"+ aid +"']");
     $('html,body').animate({scrollTop: aTag.offset().top-100},'slow');
 }
 
+// ajax call script to clear out our target
 function clearSessionTarget() {
 
-	 // trigger this function when our form runs
     $.ajax({
         url:'/system/modules/page-tag-navigation/assets/php/action.clear.session.target.php',
         type:'POST',
@@ -31,4 +43,8 @@ function clearSessionTarget() {
         }
     });
 	
+}
+
+function addBouncingBall(target_id) {
+	$("#"+target_id).prepend( '<div class="target_ball"></div>' );
 }
