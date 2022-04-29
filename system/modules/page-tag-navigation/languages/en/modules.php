09d3572a9de6bda9d70253c33c54226ff41d2cf0
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2022-04-29 11:12:25 -0400

    Update modules.php

--- system/modules/page-tag-navigation/languages/en/modules.php
+++ system/modules/page-tag-navigation/languages/en/modules.php
@@ -12,8 +12,9 @@
 **/
 
 /* Front end modules */
-$GLOBALS['TL_LANG']['FMD']['page_tag_navigation_module'] 	= array('Page Tag Navigation Module', 'The front end module that displays the Page Tag Navigation dropdowns.');
+$GLOBALS['TL_LANG']['FMD']['page_tag_navigation_module'] 			= array('Page Tag Navigation Module', 'The front end module that displays the Page Tag Navigation dropdowns.');
+$GLOBALS['TL_LANG']['FMD']['page_tag_navigation_target_module'] 	= array('Page Tag Navigation Target Module', 'Add to any page being targeted');
 
 /* Back end modules */
-$GLOBALS['TL_LANG']['MOD']['parent_category'] = array('Parent Categories', 'Define Parent Categories');
-$GLOBALS['TL_LANG']['MOD']['child_category'] = array('Child Categories', 'Define Child Categories');
+$GLOBALS['TL_LANG']['MOD']['parent_category'] 	= array('Parent Categories', 'Define Parent Categories');
+$GLOBALS['TL_LANG']['MOD']['child_category'] 	= array('Child Categories', 'Define Child Categories');
