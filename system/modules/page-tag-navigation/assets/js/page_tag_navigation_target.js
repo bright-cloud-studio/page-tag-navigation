bf4bf862c3c071420cdce193ab2779eaf5310b25
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2022-04-29 12:25:22 -0400

    Update page_tag_navigation_target.js

--- system/modules/page-tag-navigation/assets/js/page_tag_navigation_target.js
+++ system/modules/page-tag-navigation/assets/js/page_tag_navigation_target.js
@@ -1,21 +1,33 @@
 // When the page is loaded, get our target
 $( document ).ready(function() {
-    var our_target = getTarget();
-    alert("PTN_TARGET: " . our_target);
-    console.log("PTN_TARGET: " . our_target);
+    var our_target = $('#target_holder').text();
+    if(our_target != '') {
+    	scrollToAnchor(our_target);
+    	clearSessionTarget();
+    }
+    else
+    	console.log("No Target");
 });
 
-function getTarget() {
-    
-    // get target from php session
+
+function scrollToAnchor(aid){
+    var aTag = $("div[id='"+ aid +"']");
+    $('html,body').animate({scrollTop: aTag.offset().top-100},'slow');
+}
+
+function clearSessionTarget() {
+
+	 // trigger this function when our form runs
     $.ajax({
-        url:'/system/modules/page-tag-navigation/assets/php/action.get.target.php',
+        url:'/system/modules/page-tag-navigation/assets/php/action.clear.session.target.php',
         type:'POST',
+        data:"mode=1",
         success:function(result){
-        	return result;
+        	// redirect us to the success page
+        	console.log("Clear: SUCCESS");
         },
         error:function(result){
-            return "error getting session ptn_target";
+			console.log("Clear: FAILURE");
         }
     });
 	
