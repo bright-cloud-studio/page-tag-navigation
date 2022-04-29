00d592e8e54ee128e1a19fe966fda80aee288c45
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2022-04-29 11:11:48 -0400

    Update tl_module.php

--- system/modules/page-tag-navigation/dca/tl_module.php
+++ system/modules/page-tag-navigation/dca/tl_module.php
@@ -11,4 +11,5 @@
 **/
 
 /* Add a palette to tl_module */
-$GLOBALS['TL_DCA']['tl_module']['palettes']['page_tag_navigation_module'] 		= '{title_legend},name,headline,type;{template_legend:hide},customTpl;{expert_legend:hide},guests,cssID,space';
+$GLOBALS['TL_DCA']['tl_module']['palettes']['page_tag_navigation_module'] 				= '{title_legend},name,headline,type;{template_legend:hide},customTpl;{expert_legend:hide},guests,cssID,space';
+$GLOBALS['TL_DCA']['tl_module']['palettes']['page_tag_navigation_target_module'] 		= '{title_legend},name,headline,type;{template_legend:hide},customTpl;{expert_legend:hide},guests,cssID,space';
